9836b503770c643856c699dee44d0db3995e6941
SrMassive <xmassiveg@gmail.com>
2023-06-02 18:30:27 -0300

    Adição de interface

---
+++ src/main/java/bancodigital/IConta.java
@@ -0,0 +1,11 @@
+package bancodigital;
+
+public interface IConta{
+
+    public void sacar(double valor);
+
+    public void depositar(double valor);
+
+    public void transferir(double valor, Conta contaDestino);
+
+}
