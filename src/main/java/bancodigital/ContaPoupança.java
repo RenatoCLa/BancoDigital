14a15412a44b5bbad06c690e7d9eec3811972dc8
SrMassive <xmassiveg@gmail.com>
2023-06-02 18:22:34 -0300

    Criação de uma superclasse Conta

--- src/main/java/bancodigital/ContaPoupança.java
+++ src/main/java/bancodigital/ContaPoupança.java
@@ -1,35 +1,5 @@
 package bancodigital;
 
-public class ContaPoupança {
-    
-    private int agencia;
-    private int numero;
-    private double saldo;
-    
-    public void sacar(){
-
-    }
-
-    public void depositar(){
-
-    }
-
-    public void transferir(){
-        
-    }
-
-    public int getAgencia(){
-        
-        return agencia;
-    }
-
-    public int getNumero(){
-
-        return numero;
-    }
-
-    public double getSaldo(){
-
-        return saldo;
-    }
+public class ContaPoupança extends Conta {
+   
 }
